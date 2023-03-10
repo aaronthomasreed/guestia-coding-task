c6338f6129e505ce735f49461ff221c8a31ca96f
Aaron Reed <aaron.reed@vertouk.com>
2023-03-10 19:08:00 +0000

    Updated Program.cs

--- GuestiaCodingTask/Program.cs
+++ GuestiaCodingTask/Program.cs
@@ -1,7 +1,10 @@
 ﻿using GuestiaCodingTask.Data;
 using GuestiaCodingTask.Helpers;
 using System;
+using System.Data;
+using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace GuestiaCodingTask
 {
@@ -11,7 +14,36 @@ namespace GuestiaCodingTask
         {
             DbInitialiser.CreateDb();
 
+            GuestiaContext db = new GuestiaContext();
 
+            List<Guest> guests = db.Guests.Include(n => n.GuestGroup).Where(n => n.RegistrationDate == null).ToList();
+
+            foreach (var group in guests.GroupBy(g => g.GuestGroup))
+            {
+                Console.WriteLine("Unregistered " + group.Key.Name + " Guests");
+                Console.WriteLine("========================================");
+
+                IEnumerable<string> formattedGuestNames = group.Select(s => GetFormmattedName(s));
+
+                foreach (string name in formattedGuestNames.OrderBy(o => o))
+                    Console.WriteLine(name);
+
+                Console.WriteLine("========================================\n");
+            }
+
+            Console.ReadLine();
+        }
+
+        static string GetFormmattedName(Guest guest)
+        {
+            switch (guest.GuestGroup.NameDisplayFormat)
+            {
+                case NameDisplayFormatType.UpperCaseLastNameSpaceFirstName:
+                    return guest.LastName.ToUpper() + " " + guest.FirstName;
+                case NameDisplayFormatType.LastNameCommaFirstNameInitial:
+                default:
+                    return guest.LastName + ", " + guest.FirstName[0];
+            }
         }
     }
 }
